9483a959fb3b42f509f60c79763d06c5708cda9d
Abhishek Chauhan <abhishekchauhan1509@gmail.com>
2024-02-08 21:15:25 +0530

    add new note + update note + delete note added

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -9,8 +9,11 @@ const authenticateUser = require("../middlewares/authenticateUser");
 
 const router = express.Router();
 
-router.use(authenticateUser); 
+router.use(authenticateUser); // Middleware to authenticate user using JWT
 
-router.get("/notes", getNotes); 
+router.get("/notes", getNotes); // Get all notes
+router.post("/notes", addNote); // Add a new note
+router.put("/notes/:id", updateNote); // Update a note
+router.delete("/notes/:id", deleteNote); // Delete a note
 
 module.exports = router;
